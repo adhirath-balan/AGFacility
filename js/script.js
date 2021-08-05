335a31d1d75cd6ddb82b7efe2e990c8dff8d4b5e
Ezhilan B <69838801+Ezhilan-B@users.noreply.github.com>
2021-08-05 10:36:08 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -46,7 +46,6 @@ $(document).ready(function () {
 })
 
 //Contact Us
-
 function contactFunction(){
     document.getElementById("hidden-content").className = "d-inline"
 }
@@ -55,27 +54,45 @@ function hideFunction(){
     document.getElementById("hidden-content").className = "d-none"
 }
 
-// Commercial Hover
+// Grp Text Hover 
+function grpTextIn() {
+    document.getElementsByClassName("grp-text")[0].style.opacity = "1"
+    document.getElementsByClassName("grp-text")[0].style.zIndex = "7"
+}
 
+function grpTextOut() {
+    document.getElementsByClassName("grp-text")[0].style.opacity = "0"
+    document.getElementsByClassName("grp-text")[0].style.zIndex = "0"
+}
+
+// Commercial Hover
 function commercialIn(){
     document.getElementsByClassName("res-hover")[0].style.zIndex = "1"
     document.getElementsByClassName("hover_center")[0].style.zIndex = "0"
+    document.getElementsByClassName("dots")[0].classList.add("d-none")
+    document.getElementsByClassName("expand")[0].classList.remove("d-none")
 }
 
 function commercialOut(){
     document.getElementsByClassName("res-hover")[0].style.zIndex = "-1"
     document.getElementsByClassName("hover_center")[0].style.zIndex = "4"
+    document.getElementsByClassName("dots")[0].classList.remove("d-none")
+    document.getElementsByClassName("expand")[0].classList.add("d-none")
 }
 
 // Home Hover
 
 function homeIn(){
     document.getElementsByClassName("res-hover")[1].style.zIndex = "3"
+    document.getElementsByClassName("dots")[1].classList.add("d-none")
+    document.getElementsByClassName("expand")[1].classList.remove("d-none")
 }
 
 function homeOut(){
     document.getElementsByClassName("res-hover")[1].style.zIndex = "-1"
-}
+    document.getElementsByClassName("dots")[1].classList.remove("d-none")
+    document.getElementsByClassName("expand")[1].classList.add("d-none")
+ }
 
 // Plumbing Hover
 
@@ -83,12 +100,16 @@ function plumbingIn(){
     document.getElementsByClassName("res-hover")[2].style.zIndex = "1"
     document.getElementsByClassName("hover_center")[1].style.zIndex = "0"
     document.getElementsByClassName("hover_right")[0].style.zIndex = "0"
+    document.getElementsByClassName("dots")[2].classList.add("d-none")
+    document.getElementsByClassName("expand")[2].classList.remove("d-none")
 }
 
 function plumbingOut(){
     document.getElementsByClassName("res-hover")[2].style.zIndex = "-1"
     document.getElementsByClassName("hover_center")[1].style.zIndex = "4"
     document.getElementsByClassName("hover_right")[0].style.zIndex = "6"
+    document.getElementsByClassName("dots")[2].classList.remove("d-none")
+    document.getElementsByClassName("expand")[2].classList.add("d-none")
 }
 
 // Electrical Hover
@@ -96,21 +117,29 @@ function plumbingOut(){
 function electricalIn(){
     document.getElementsByClassName("res-hover")[3].style.zIndex = "3"
     document.getElementsByClassName("hover_right")[0].style.zIndex = "0"
+    document.getElementsByClassName("dots")[3].classList.add("d-none")
+    document.getElementsByClassName("expand")[3].classList.remove("d-none")
 }
 
 function electricalOut(){
     document.getElementsByClassName("res-hover")[3].style.zIndex = "-1"
     document.getElementsByClassName("hover_right")[0].style.zIndex = "6"
+    document.getElementsByClassName("dots")[3].classList.remove("d-none")
+    document.getElementsByClassName("expand")[3].classList.add("d-none")
 }
 
 // Sewage Hover
 
 function sewageIn(){
     document.getElementsByClassName("res-hover")[4].style.zIndex = "5"
+    document.getElementsByClassName("dots")[4].classList.add("d-none")
+    document.getElementsByClassName("expand")[4].classList.remove("d-none")
 }
 
 function sewageOut(){
     document.getElementsByClassName("res-hover")[4].style.zIndex = "-1"
+    document.getElementsByClassName("dots")[4].classList.remove("d-none")
+    document.getElementsByClassName("expand")[4].classList.add("d-none")
 }
 
 // Security Hover
@@ -119,12 +148,16 @@ function securityIn(){
     document.getElementsByClassName("res-hover")[5].style.zIndex = "1"
     document.getElementsByClassName("hover_center")[2].style.zIndex = "0"
     document.getElementsByClassName("hover_right")[1].style.zIndex = "0"
+    document.getElementsByClassName("dots")[5].classList.add("d-none")
+    document.getElementsByClassName("expand")[5].classList.remove("d-none")
 }
 
 function securityOut(){
     document.getElementsByClassName("res-hover")[5].style.zIndex = "-1"
     document.getElementsByClassName("hover_center")[2].style.zIndex = "4"
     document.getElementsByClassName("hover_right")[1].style.zIndex = "6"
+    document.getElementsByClassName("dots")[5].classList.remove("d-none")
+    document.getElementsByClassName("expand")[5].classList.add("d-none")
 }
 
 // Painting Hover
@@ -132,19 +165,27 @@ function securityOut(){
 function paintingIn(){
     document.getElementsByClassName("res-hover")[6].style.zIndex = "3"
     document.getElementsByClassName("hover_right")[1].style.zIndex = "0"
+    document.getElementsByClassName("dots")[6].classList.add("d-none")
+    document.getElementsByClassName("expand")[6].classList.remove("d-none")
 }
 
 function paintingOut(){
     document.getElementsByClassName("res-hover")[6].style.zIndex = "-1"
     document.getElementsByClassName("hover_right")[1].style.zIndex = "6"
+    document.getElementsByClassName("dots")[6].classList.remove("d-none")
+    document.getElementsByClassName("expand")[6].classList.add("d-none")
 }
 
 // Furniture Hover
 
 function furnitureIn(){
     document.getElementsByClassName("res-hover")[7].style.zIndex = "5"
+    document.getElementsByClassName("dots")[7].classList.add("d-none")
+    document.getElementsByClassName("expand")[7].classList.remove("d-none")
 }
 
 function furnitureOut(){
     document.getElementsByClassName("res-hover")[7].style.zIndex = "-1"
+    document.getElementsByClassName("dots")[7].classList.remove("d-none")
+    document.getElementsByClassName("expand")[7].classList.add("d-none")
 }
