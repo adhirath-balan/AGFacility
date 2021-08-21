e89814d9a19d184982580d0a4bd3ecb6d0434682
Ezhilan B <66116632+adhirath-balan@users.noreply.github.com>
2021-08-22 00:48:19 +0530

    Slider added

--- js/script.js
+++ js/script.js
@@ -390,3 +390,29 @@ function closeGallery() {
     document.getElementsByClassName("gal-section")[0].classList.add("d-none");
 }
 
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-out");
+
+const appearOptions = {
+    threshold: 1
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+entries.forEach(entry => {
+    if(!entry.isIntersecting){
+        return;
+    }else{
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+    }
+});
+},
+appearOptions);
+
+faders.forEach(fader =>{
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
